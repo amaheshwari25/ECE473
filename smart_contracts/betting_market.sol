a4d0cbe2cbfa40210110e266ad16fc0e69080e0d
agangavaram <abiramgupta8@gmail.com>
2023-04-24 23:28:58 -0400

    contract compiles

--- smart_contracts/betting_market.sol
+++ smart_contracts/betting_market.sol
@@ -1,23 +1,22 @@
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
+import { SD59x18, sd, exp, ln, intoInt256} from "./SD59x18.sol";
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./token.sol";
 import "./oracle.sol";
 
 contract PredictionMarket {
-    using SafeMath for uint256;
 
     Token token;
     Oracle public oracle;
-    uint256 constant PRECISION = 10 ** 18;
+
     uint256 outcomeCount;
-    uint256[] quantities;
-    uint256 alpha;
-    uint256 beta;
+    SD59x18[] quantities;
+    SD59x18 alpha;
+    SD59x18 beta;
     string msr;
 
     struct Trader {
-        uint256[] payouts;
+        SD59x18[] payouts;
     }
 
     mapping(address => Trader) internal traders;
@@ -26,120 +25,107 @@ contract PredictionMarket {
 
     constructor(
         uint256 _outcomeCount,
-        uint256 _alpha,
-        uint256 _beta,
-        Oracle _oracle,
+        SD59x18 _alpha,
+        SD59x18 _beta,
+       // Oracle _oracle,
         string memory _msr,
         Token _token
-    ) public {
+    )  {
         outcomeCount = _outcomeCount;
-        alpha = _alpha;
-        beta = _beta;
-        oracle = _oracle;
+        alpha = _alpha / sd(1e5);
+        beta = _beta / sd(1e5);
+      //  oracle = _oracle;
         msr = _msr;
         token = _token;
 
-        quantities = new uint[](_outcomeCount);
+        quantities = new SD59x18[](_outcomeCount);
         for (uint256 i = 0; i < outcomeCount; i++) {
-            quantities[i] = 100 * PRECISION; // initialize to 100
+            quantities[i] = sd(100) ; // initialize to 100
         }
     }
 
-    function get_price_vector() public view returns (uint256[] memory) {
-        uint256[] memory prices = new uint256[](outcomeCount);
+    function get_price_vector() public view returns (int256[] memory) {
+        int256[] memory prices = new int[](outcomeCount);
         for (uint256 i = 0; i < outcomeCount; i++) {
             prices[i] = get_price(i, quantities);
         }
+       
         return prices;
+    }
+
+    function sum(SD59x18[] memory array) private pure returns (SD59x18) {
+        SD59x18 s = sd(0);
+        for(uint i = 0; i < array.length; i++) {
+            s = s + array[i];
+        }
+        return s;
     }
 
     function get_price(
         uint256 i,
-        uint256[] memory q
-    ) public view returns (uint256) {
-        if (msr == "LMSR") {
-            uint256 numerator = SafeMath.exp(q[i]) / PRECISION;
-            uint256 denominator = 0;
-            for (uint256 j = 0; j < outcomeCount; j++) {
-                denominator = denominator.add(SafeMath.exp(q[j]) / PRECISION);
-            }
-            return numerator.mul(PRECISION).div(denominator);
+        SD59x18[] memory q
+    ) public view returns (int256) {
+        if (keccak256(abi.encodePacked(msr)) == keccak256(abi.encodePacked("LMSR"))) {
+            return intoInt256(((q[i] / beta).exp()) / sum(q));
+            
         } else {
             // "LS-LMSR"
+            SD59x18 b = alpha * sum(q);
+            SD59x18[] memory q_temp = new SD59x18[](q.length);
+            for(uint j = 0; j < outcomeCount; j++) {
+                q_temp[j] = (q[j] / b).exp();
+            }
+            SD59x18 sum_term = sum(q_temp);
+            SD59x18 term1 = alpha * sum_term.ln();
+            for(uint j = 0; j < outcomeCount; j++) {
+                q_temp[j] = q[j] * (q[j]/b).exp();
+            }
+            SD59x18 term2_num = sum(q) * (q[i] / b).exp() - sum(q_temp);
+            SD59x18 term2_denom = sum(q) * sum_term;
 
-            uint256 totalShares = 0;
-            for (uint j = 0; i < outcomeCount; j++) {
-                totalShares += q[i];
-            }
-            uint256 b = alpha.mul(totalShares);
-            uint256 sumTerm = 0;
-            for (uint256 j = 0; j < outcomeCount; j++) {
-                sumTerm = sumTerm.add(SafeMath.exp(q[j].div(b)));
-            }
-            uint256 term1 = alpha.mul(sumTerm.log());
-            uint256 term2Num = totalShares.mul(q[i].div(b).exp()).sub(
-                sumExponential(q, b)
-            );
-            uint256 term2Denom = totalShares.mul(sumTerm);
-            return term1.add(term2Num.mul(PRECISION).div(term2Denom));
+            return intoInt256(term1 + (term2_num / term2_denom));
         }
     }
 
-    function sumExponential(
-        uint256[] memory q,
-        uint256 b
-    ) internal pure returns (uint256) {
-        uint256 sum = 0;
-        for (uint256 j = 0; j < q.length; j++) {
-            sum = sum.add(SafeMath.exp(q[j].mul(q[j].div(b))));
-        }
-        return sum;
-    }
 
     function cost_LMSR(
-        uint256 b,
-        uint256[] memory q
-    ) internal pure returns (uint256) {
-        uint256 totalExp = 0;
+        SD59x18 b,
+        SD59x18[] memory q
+    ) internal view returns (SD59x18) {
+        SD59x18 totalExp = sd(0);
         for (uint256 i = 0; i < outcomeCount; i++) {
-            totalExp = totalExp.add(
-                SafeMath.exp(quantities[i].mul(PRECISION).div(b)).div(PRECISION)
-            );
+            totalExp = totalExp + (q[i] / b).exp();
         }
-        return b.mul(totalExp.ln().mul(PRECISION)).div(PRECISION);
+        return b * totalExp.ln();
     }
 
-    function cost(uint256[] memory q) public view returns (uint256) {
-        if (msr == "LMSR") {
+    function cost(SD59x18[] memory q) public view returns (SD59x18) {
+        if (keccak256(abi.encodePacked(msr)) == keccak256(abi.encodePacked("LMSR"))) {
             return cost_LMSR(beta, q);
         } else {
-            // "LS-LMSR"
-            uint256 qTotal = 0;
-            for (uint256 i = 0; i < outcomeCount; i++) {
-                qTotal += quantities[i];
-            }
-            uint256 b = alpha.mul(qTotal);
+            // "LS-LMSR"          
+            SD59x18 b = alpha * sum(q);
             return cost_LMSR(b, q);
         }
     }
 
-    function submitBet(uint i, uint shares) public payable {
-        uint256[] memory delta = new uint[](outcomeCount);
+    function submitBet(uint i, SD59x18 shares) public payable {
+        SD59x18[] memory delta = new SD59x18[](outcomeCount);
         delta[i] = shares;
 
-        uint256[] memory oldQ = quantities;
-        uint256[] memory newQ = new uint[](outcomeCount);
+        SD59x18[] memory oldQ = quantities;
+        SD59x18[] memory newQ = new SD59x18[](outcomeCount);
         for (uint j = 0; j < outcomeCount; j++) {
             newQ[j] = oldQ[j] + delta[j];
         }
 
-        uint256 oldCost = cost(oldQ);
-        uint256 newCost = cost(newQ);
-        uint256 costDiff = newCost.sub(oldCost);
+        SD59x18 oldCost = cost(oldQ);
+        SD59x18 newCost = cost(newQ);
+        int256 costDiff = intoInt256(newCost - oldCost);
 
         // Transfer funds from user's wallet to contract's wallet
         require(
-            token.transferFrom(msg.sender, address(this), costDiff),
+            token.transferFrom(msg.sender, address(this), uint(costDiff)),
             "Transfer failed"
         );
 
@@ -149,14 +135,15 @@ contract PredictionMarket {
         //  require(outcomeTokens[i].transfer(msg.sender, shares), "Transfer failed");
     }
 
-    function redeem_winnings() public {
-        uint256[] memory payouts = traders[msg.sender].payouts;
-        uint256 totalPayout = 0;
+    function redeem_winnings() public payable {
+        SD59x18[] memory payouts = traders[msg.sender].payouts;
+        SD59x18 totalPayout = sd(0);
         for (uint256 i = 0; i < outcomeCount; i++) {
-            totalPayout = totalPayout.add(payouts[i]);
+            totalPayout = totalPayout + payouts[i];
         }
-        require(totalPayout > 0, "No winnings to redeem");
-        traders[msg.sender].payouts = new uint256[](outcomeCount);
-        msg.sender.transfer(totalPayout);
+        int256 payout = intoInt256(totalPayout);
+        require(intoInt256(totalPayout) > 0, "No winnings to redeem");
+        traders[msg.sender].payouts = new SD59x18[](outcomeCount);
+        token.transferFrom(address(this), msg.sender, uint(payout));
     }
 }
